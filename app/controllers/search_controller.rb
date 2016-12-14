94f473b14a0babea67affbf6cfb236caf722da57
Megan Talbot <meganftalbot@gmail.com>
2016-12-14 10:03:54 -0700

    Add services folder for Nrel service, not working yet to back in controller. View not yet rendering

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,16 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get( "http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["nrel_api_key"]}&location=80202&fuel_type=ELEC&LPG&offset=10
-")
+    response = Faraday.get( "http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["nrel_api_key"]}&location=80202&fuel_type=ELEC&LPG&offset=10")
 
     data = JSON.parse(response.body)
-    @result = data["fuel_stations"].first["station_name"]
-byebug
+    result = {}
+
+    result["name"] = data["fuel_stations"].first["station_name"]
+    result["addresss"] = data["fuel_stations"].first["street_address"]
+    result["fuel_type"] = data["fuel_stations"].first["fuel_type_code"]
+    result["distance"] = data["fuel_stations"].first["distance"]
+    result["access_times"] = data["fuel_stations"].first["access_days_times"]
+
+    @result
   end
 end
---
+++ app/services/nrel _service.rb
@@ -0,0 +1,7 @@
+class NrelService
+
+#   def get_nearest_fuel
+#     response = Faraday.get( "http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["nrel_api_key"]}&location=80202&fuel_type=ELEC&LPG&offset=10
+# ")
+#   end
+end
