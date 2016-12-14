3d0675472df4c2495943aa62f48e62968d23026b
Megan Talbot <meganftalbot@gmail.com>
2016-12-14 09:31:27 -0700

    Add searh controller, begin with Faraday response

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,7 @@ require 'vcr'
 
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-  config.hook_into :webmock # or :fakeweb
+  config.hook_into :webmock 
   config.configure_rspec_metadata!
 end
 #
